b24675faa996ac6364f4b67f20acee291892f8ce
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-22 19:14:40 -0400

    changed fetchtest

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 console.log("It's alive");
 
 async function fetchTest() {
-    const res = await fetch("http://localhost/");
-    data = await res.json();
     console.log(data)
     console.log(PORT);
     console.log(process.env.PORT)
