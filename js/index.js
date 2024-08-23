4f2d00fe881a62cdf8886c7953df21ae27887514
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-22 23:52:54 -0400

    added logic to index.js that will display functions and payloads correctly

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
+/*----- Global Variables ------*/
 console.log("It's alive");
 const API_URL = "https://functions-api.shopifydemo.shop";
 
 /*----- State ------*/
 let functions;
-const functionSelectEl = document.querySelector("select.function-type")
+let selectedFunction;
+let selectedScenario;
+const functionSelectEl = document.querySelector("select.function-type");
+const scenarioSelectEl = document.querySelector("select.function-scenario");
+const jsonPayloadEl = document.querySelector("textarea.json-payload");
 
+/*----- Event Handlers ------*/
+functionSelectEl.addEventListener("change", (evt) => {
+  selectedFunction = functions.find(
+    (fun) => fun.functionEndpoint === evt.target.value
+  );
+  render();
+});
 
+scenarioSelectEl.addEventListener("change", (evt) => {
+  selectedScenario = selectedFunction.scenarios.find(
+    (scenario) => scenario.scenarioEndpoint === evt.target.value
+  );
+  render();
+});
+
+/*----- Intalization ------*/
 fetchFunctions();
 
 async function fetchFunctions() {
@@ -15,11 +35,46 @@ async function fetchFunctions() {
   render();
 }
 
+/*----- Render ------*/
 function render() {
+  // defaulting inner HTML to blank text
   functionSelectEl.innerHTML = "";
+  scenarioSelectEl.innerHTML = "";
+  jsonPayloadEl.innerHTML = "";
+
+  // defaulting selected options to first option if nothing has been selected
+  selectedFunction ? selectedFunction : (selectedFunction = functions[0]);
+  selectedScenario ? selectedScenario : (selectedScenario = functions[0].scenarios[0]);
+
   functions.forEach((fun) => {
-    const optionEl = document.createElement("option");
-    optionEl.textContent = fun.functionName;
-    functionSelectEl.append(optionEl);
+    if (fun.display) {
+      const optionEl = document.createElement("option");
+      optionEl.textContent = fun.functionName;
+      optionEl.value = fun.functionEndpoint;
+      functionSelectEl.append(optionEl);
+      if (fun.functionEndpoint === selectedFunction.functionEndpoint)
+        optionEl.selected = true;
+    }
   });
+
+  if (!selectedFunction.scenarios.includes(selectedScenario)){
+    selectedScenario = selectedFunction.scenarios[0]
+  }
+
+  selectedFunction.scenarios.forEach((scenario) => {
+    if (scenario.display) {
+      const optionEl = document.createElement("option");
+      optionEl.textContent = scenario.scenarioName;
+      optionEl.value = scenario.scenarioEndpoint;
+      scenarioSelectEl.append(optionEl);
+      if (scenario.scenarioEndpoint === selectedScenario.scenarioEndpoint)
+        optionEl.selected = true;
+    }
+  });
+
+  jsonPayloadEl.textContent = JSON.stringify(
+    selectedScenario.samplePayload,
+    null,
+    1
+  );
 }
