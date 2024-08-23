d09f25fdc47e7f9ab4bd4b1d343864e17c3e2cd3
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-22 20:06:29 -0400

    updated index.js with callout function

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("It's alive");
 const API_URL = "https://functions-api.shopifydemo.shop/";
 
 async function fetchFunctions() {
-  const res = await fetch(`${BASE_URL}/functions`);
+  const res = await fetch(`${API_URL}/functions`);
   functionsData = await res.json();
   console.log(functionsData);
 }
