53cee4b1268a00903c95e381df695012fe3cbe20
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-22 20:39:13 -0400

    updated css and updated fetch and render js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 console.log("It's alive");
 const API_URL = "https://functions-api.shopifydemo.shop";
 
-async function fetchFunctions() {
-  const res = await fetch(`${API_URL}/functions`);
-  functionsData = await res.json();
-  console.log(functionsData);
-}
+/*----- State ------*/
+let functions;
+const functionSelectEl = document.querySelector("select.function-type")
+
 
 fetchFunctions();
+
+async function fetchFunctions() {
+  const res = await fetch(`${API_URL}/functions`);
+  functions = await res.json();
+  console.log(functions);
+  render();
+}
+
+function render() {
+  functionSelectEl.innerHTML = "";
+  functions.forEach((fun) => {
+    const optionEl = document.createElement("option");
+    optionEl.textContent = fun.functionName;
+    functionSelectEl.append(optionEl);
+  });
+}
